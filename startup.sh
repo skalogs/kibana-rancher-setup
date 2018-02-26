31086f528e5200aebfdb249041281aaa38494be0
christophefromparis <christophe.cosnefroy@gmail.com>
2018-02-26 15:42:08 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -14,8 +14,7 @@ KIBANA_URL=http://${ES_AUTH}${SERVICE_KIBANA_HOST}:${SERVICE_KIBANA_PORT}
 function checkElasticsearch {
     curlCommand="curl --silent ${ES_URL}/_cluster/health -H 'Content-Type: application/json;charset=UTF-8' --write-out %{http_code} --output /dev/null"
     status=$(eval $curlCommand)
-    while  [ $status -ne 200 ] ;
-    do
+    while  [ $status -ne 200 ] ; do
         status=$(eval $curlCommand)
         echo "I'm waiting Elasticsearch"
 	sleep 5
@@ -25,8 +24,7 @@ function checkElasticsearch {
 function checkKibana {
     curlCommand="curl --silent ${KIBANA_URL}/api/status -H 'Content-Type: application/json;charset=UTF-8' --write-out %{http_code} --output /dev/null"
     status=$(eval $curlCommand)
-    while  [ $status -ne 200 ] ;
-    do
+    while  [ $status -ne 200 ] ; do
         status=$(eval $curlCommand)
 	echo "I'm waiting Kibana"
         sleep 5
@@ -40,7 +38,7 @@ INDEX_PATTERN=$(curl --silent ${RANCHER_BASEURL}/self/service/metadata/index-pat
 
 if [ ! -z "${INDEX_PATTERN}" ]; then
     while read INDEX TIME_FIELD ; do
-        echo "Create index pattern"
+        echo "\nCreate index pattern"
         curl -f -XPOST "$KIBANA_URL/api/saved_objects/index-pattern/$INDEX" \
              -H "Content-Type: application/json" \
              -H "kbn-xsrf: anything" \
@@ -49,7 +47,7 @@ if [ ! -z "${INDEX_PATTERN}" ]; then
     done <<< "$INDEX_PATTERN"
 
     if [ ! -z "${DEFAULT_INDEX_PATTERN}" ]; then
-        echo "Setting default index pattern"
+        echo "\nSetting default index pattern"
         curl ${KIBANA_URL}/api/kibana/settings/defaultIndex \
 	         -H "Content-Type: application/json" \
 	         -H "kbn-xsrf: anything" \
